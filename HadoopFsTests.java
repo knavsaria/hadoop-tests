29b486471733fba3922e1bf6c4982b5d8b444609
keeran.navsaria@gmail.com <knnavsar@8c85903c1283.ant.amazon.com>
2019-02-12 09:37:00 +0200

    updated to version 2

--- HadoopFsTests.java
+++ HadoopFsTests.java
@@ -1,5 +1,5 @@
 public class HadoopFsTests {
 	public static void main (String[] args) {
-		System.out.println("testing jar file creation version 2");
+		System.out.println("testing jar file creation version 3");
 	}
 }
