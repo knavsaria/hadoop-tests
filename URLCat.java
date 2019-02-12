4e9e84d8750b734eb2e612d2a4d8308311cb0bbf
keeran.navsaria@gmail.com <knnavsar@8c85903c1283.ant.amazon.com>
2019-02-12 09:55:52 +0200

    sdasdsa

--- URLCat.java
+++ URLCat.java
@@ -1,5 +1,16 @@
-public class HadoopFsTests {
-	public static void main (String[] args) {
-		System.out.println("testing jar file creation version 3");
-	}
-}
+public class URLCat {
+
+    static {
+      URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+    
+    public static void main(String[] args) throws Exception {
+      InputStream in = null;
+      try {
+        in = new URL(args[0]).openStream();
+        IOUtils.copyBytes(in, System.out, 4096, false);
+      } finally {
+        IOUtils.closeStream(in);
+      }
+    }
+  }
